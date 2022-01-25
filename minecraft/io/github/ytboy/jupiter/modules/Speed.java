f8c5726f56a95a8e4cf30f4c4df6d501a1aaa754
Torono909 <86355565+Torono909@users.noreply.github.com>
2022-01-25 11:58:14 +0530

    Update Speed.java

--- minecraft/io/github/ytboy/jupiter/modules/Speed.java
+++ minecraft/io/github/ytboy/jupiter/modules/Speed.java
@@ -1,1 +1,18 @@
 package io.github.ytboy.jupiter.modules;
+package io.github.ytboy.jupiter.modules;
+
+import org.lwjgl.input.Keyboard;
+
+import com.darkmagician6.eventapi.EventManager;
+import com.darkmagician6.eventapi.EventTarget;
+
+import io.github.ytboy.jupiter.event.EventPostUpdate;
+import io.github.ytboy.jupiter.event.EventPreUpdate;
+import io.github.ytboy.jupiter.module.Module;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.play.client.CPacketAnimation;
+
+public class Speed extends Module {
+	public Speed() {
+		super("Speed", "Like Timer but only bypass with the right settings.", Keyboard.KEY_O, Category.MOVEMENT);
+  }
